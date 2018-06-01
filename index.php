8758ee98ef8ed77f273148bab2f35a742515fb32
Crepu <danielmendez310@gmail.com>
2018-06-01 15:01:32 -0400

    Test Database conection

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
-
-print "Hola Mundo!"
-
+$hostname_DB = "localhost";
+$database_DB = "app_test";
+$username_DB = "app_user";
+$password_DB = "app_password";
+mysql_connect($hostname_DB,$username_DB,$password_DB);
+@mysql_select_db($database_DB) or die( "Unable to select database");
+$query="SELECT * FROM app_test";
+$result=mysql_query($query);
+$num=mysql_numrows($result);
+mysql_close();
+echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center></b><br><br>";
+$i=0;
+while ($i < $num) {
+$name=mysql_result($result,$i,"name");
+$value=mysql_result($result,$i,"value");
+echo "<b>$name:$value</b><br><hr><br>";
+$i++;
+}
 ?>
