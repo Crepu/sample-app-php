8c96f9bfaf558e4fc622fc4e923161bca428f085
Crepu <danielmendez310@gmail.com>
2018-06-08 18:43:58 -0400

    First app

--- lbserver/indexp.php
+++ lbserver/indexp.php
@@ -29,24 +29,10 @@
   </li>
 </ul>
 </div>
-<?php
-include '../config/db.php';
-
-mysql_connect($hostname_DB,$username_DB,$password_DB);
-mysql_select_db($database_DB) or die( "Unable to select database");
-$query="SELECT * FROM app_test";
-$result=mysql_query($query);
-$num=mysql_numrows($result);
-mysql_close();
-echo "<b><center> Database($database_DB) Output From Host($hostname_DB)</center></b><br><br>";
-$i=0;
-while ($i < $num) {
-$name=mysql_result($result,$i,"name");
-$value=mysql_result($result,$i,"value");
-echo "<b>$name:$value</b><br><hr><br>";
-$i++;
-}
-?>
+<div> 
+<object type="text/html" data="http://137.135.126.29/haproxy-status" width="800px" height="600px" style="overflow:auto;border:5px ridge blue">
+</object>
+</div>
 </div>
 
 </body>
