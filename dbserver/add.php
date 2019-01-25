fc563af640a46ed781bc84c431124e534bedc828
Crepu <danielmendez310@gmail.com>
2019-01-25 17:27:21 -0300

    App lista

--- dbserver/add.php
+++ dbserver/add.php
@@ -3,12 +3,12 @@
 
     mysql_connect($hostname_DB,$username_DB,$password_DB);
     mysql_select_db($database_DB) or die( "Unable to select database");
-    $query="INSERT INTO app_test (name, value) VALUES ($_POST[name], $_POST[value])";
+    $query="INSERT INTO app_test (name, value) VALUES ('$_POST[name]', '$_POST[value]')";
     $result=mysql_query($query);
     if($result === TRUE){
-        header('Location: index.php')
+        header('Location: index.php');
     } else {
-    echo "Error"
+        echo "Error".mysql_error();
     }
     mysql_close();
 ?>
